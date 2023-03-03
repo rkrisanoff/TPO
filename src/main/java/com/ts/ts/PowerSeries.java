119ff5493baa20c86b6767abf4c24514df7d7adb
Anton-Shabalov <anton.shabalov@icloud.com>
2023-03-03 20:28:54 +0300

    корректировка имени константы

--- src/main/java/com/ts/ts/PowerSeries.java
+++ src/main/java/com/ts/ts/PowerSeries.java
@@ -4,7 +4,7 @@ import static java.lang.Math.abs;
 import static java.lang.Math.pow;
 
 public class PowerSeries {
-    final static double  eps = 0.000001d;
+    final static double EPS = 0.000001d;
     public static double calculateArctg(double arg, int n) throws IllegalArgumentException {
         if (abs(arg) > 1) {
             throw new IllegalArgumentException("Argument "+arg+" is out of definition are");
@@ -18,7 +18,7 @@ public class PowerSeries {
             result += part ;
             part = pow(-1, i - 1 ) * pow(arg, i * 2 - 1) / (i * 2 - 1);
             i += 1;
-        } while ((abs(part) > eps) && (i <= n));
+        } while ((abs(part) > EPS) && (i <= n));
 
         return result;
     }
